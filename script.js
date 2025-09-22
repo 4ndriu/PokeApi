c61ddeea734d478328fe3181e92526a5d110abc0
4ndriu <andres.castillop@uniagustiniana.edu.co>
2025-09-22 12:42:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function revealInfo() {
+  const text = document.querySelector('.text');
+  const button = document.querySelector('button');
 
+  // Revela información secreta
+  text.textContent = 'Información adicional: La investigación está relacionada con un posible ciberataque a nivel global. Las pruebas aún están bajo análisis, pero se ha identificado una IP sospechosa. Manténgase atento a nuevas actualizaciones.';
+  button.style.display = 'none';
+}
